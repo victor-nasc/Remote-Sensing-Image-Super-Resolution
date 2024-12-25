53fdca1ce7e214d3a8b62be58c3f0e56a1d8fe32
victor-nasc <victor_nascimento@usp.br>
2024-12-24 21:54:19 -0300

    add dataloader

--- WGAN/processing/dataloader.py
+++ WGAN/processing/dataloader.py
@@ -1,24 +1,34 @@
 import os
+import random
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 class SRDataset(Dataset):
-    def __init__(self, dir):
-        self.hr_dir = dir
+    def __init__(self, hr_dir, crop_size=128, scale_factor=4, test=False):
+        self.hr_dir = hr_dir
+        self.crop_size = crop_size
+        self.scale_factor = scale_factor
+        self.test = test
         self.hr_imgs = sorted(os.listdir(self.hr_dir))
-        self.transform = transforms.Compose([transforms.ToTensor()])
-        self.scale_factor = 4
-
 
     def __len__(self):
         return len(self.hr_imgs)
 
     def __getitem__(self, idx):
         hr_img = Image.open(os.path.join(self.hr_dir, self.hr_imgs[idx]))
-        lr_img = self._downscale(hr_img)
 
-        return self.transform(lr_img), self.transform(hr_img)
+        if not self.test:
+            hr_img = transforms.RandomCrop(self.crop_size)(hr_img)
+                
+            degree = random.choice([0, 90, 180, 270])
+            hr_img = hr_img.rotate(degree)
+        
+        lr_crop = self._downscale(hr_img)
+
+        transform = transforms.Compose([transforms.ToTensor()])
+        return transform(lr_crop), transform(hr_img)
     
     def _downscale(self, img):
-        return img.resize((img.width // self.scale_factor, img.height // self.scale_factor), Image.BICUBIC)
+        new_size = (img.width // self.scale_factor, img.height // self.scale_factor)
+        return img.resize(new_size, Image.BICUBIC)
