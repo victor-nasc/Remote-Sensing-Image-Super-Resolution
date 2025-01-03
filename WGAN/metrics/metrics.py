be635ca662b1ae496a0639ea78401bb922240c8a
victor-nasc <victor_nascimento@usp.br>
2025-01-03 10:06:10 -0300

    update

--- WGAN/metrics/metrics.py
+++ WGAN/metrics/metrics.py
@@ -1,5 +1,8 @@
 import torch
+import torchvision.models as models
+
 from pytorch_msssim import ssim
+from WGAN.metrics.perceptual_VGG import PerceptualVGG
 
 
 def wass_loss(HR: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
@@ -29,3 +32,23 @@ def PSNR(HR: torch.Tensor, fake: torch.Tensor, data_range: float = 1.0) -> torch
     psnr = 20 * torch.log10(data_range / torch.sqrt(mse))
 
     return psnr
+
+def VGG_loss(HR: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
+    device = HR.device
+
+    mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(device)
+    std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(device)
+
+    HR = (HR - mean) / std
+    fake = (fake - mean) / std
+
+    perceptual_model = PerceptualVGG(layers=['relu_1', 'relu_2', 'relu_3']).to(device)
+
+    HR_vgg = perceptual_model(HR)
+    fake_vgg = perceptual_model(fake)
+
+    loss = 0.0
+    for key in HR_vgg.keys():
+        loss += torch.mean((HR_vgg[key] - fake_vgg[key]) ** 2)
+
+    return loss
